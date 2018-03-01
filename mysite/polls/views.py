331b3789730cf9d4e6c6f6c8f372ca398e8ce956
Deepashree Kedia <deep@Deepashrees-Air.Dlink>
2018-03-01 18:41:53 +0530

    separating views from python code using templates

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,8 +1,47 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+from .models import Question
+from django.http import Http404
+#from django.template import loader
 
 # Create your views here.
 
 
+# def index(request):
+# 	latest_polls = Question.objects.order_by('-pub_date')[:5]
+# 	template = loader.get_template("polls/index.html")
+# 	context = {
+# 			'latest_polls': latest_polls,
+# 	}
+	
+# 	return HttpResponse(template.render(context, request))
+
 def index(request):
-	return HttpResponse("Hello world. At polls index.")
+	latest_polls = Question.objects.order_by('-pub_date')[:5]
+	context = {
+			'latest_polls': latest_polls,
+	}
+	
+	return render(request, 'polls/index.html', context)
+
+
+
+# def detail(request, question_id):
+# 	try:
+# 		question = Question.objects.get(pk=queston_id)
+# 	except Question.DoesnotExist
+# 		raise 	Http404("this question doesnot exists")
+#     return render(request, 'polls/detail.html', {'question': question})
+
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
+
